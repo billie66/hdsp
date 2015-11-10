512b0626e48b18e1f160f5e2f3100cb28aad46a0
Peter Wang <happypeter1983@gmail.com>
2015-11-10 10:52:04 +0800

    try react-router

---
+++ client/App.jsx
@@ -0,0 +1,9 @@
+App = React.createClass({
+  render() {
+    return (
+      <div>
+        This is App.jsx
+      </div>
+    );
+  }
+});
--- client/lib/app.browserify.js
+++ client/lib/app.browserify.js
@@ -1,3 +1,4 @@
 var injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
 mui = require('material-ui');
+ReactRouter = require("react-router");
