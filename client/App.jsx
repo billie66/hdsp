09f8f4ecee44b3dde06dd8d010723a3c3d04ca02
Peter Wang <happypeter1983@gmail.com>
2015-11-12 19:54:32 +0800

    add route

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,17 @@
 const { Tab, Tabs } = mui;
 
 App = React.createClass({
+  componentWillMount() {
+    this.setState({
+      tabIndex: this._getSelectedIndex()
+    });
+  },
   render() {
     return (
       <div>
-        <Tabs>
+        <Tabs
+          value={this.state.tabIndex}
+          onChange={this._handleTabsChange}>
           <Tab
             value='1'
             label='Home'
@@ -24,5 +31,15 @@ App = React.createClass({
         {this.props.children}
       </div>
     );
+  },
+  _getSelectedIndex() {
+    return this.props.history.isActive('/home') ? '1' :
+      this.props.history.isActive('/blog') ? '2' :
+      this.props.history.isActive('/about') ? '3' : '0';
+  },
+
+  _handleTabsChange(value, e, tab) {
+    this.props.history.pushState(null, tab.props.route);
+    this.setState({tabIndex: this._getSelectedIndex()});
   }
 });
